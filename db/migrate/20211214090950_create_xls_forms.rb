ddedc016446f517f4a2896189322fbf42fa63fcf
kspich <36812299+kspich@users.noreply.github.com>
2021-12-21 11:08:07 +0300

    илюх я не могу проверить создается юзер или нет потому что теперь хлс форма теперь не создается там вроде че то поправить надо в контроллере хлс формс

--- db/migrate/20211214090950_create_xls_forms.rb
+++ db/migrate/20211214090950_create_xls_forms.rb
@@ -1,10 +1,15 @@
 class CreateXlsForms < ActiveRecord::Migration[6.1]
   def change
-    create_table :xls_forms do |t|
+    create_table :users do |t|
       t.string :name
       t.string :email
+      t.timestamps
+    end
+
+    create_table :xls_forms do |t|
+      t.string :name
+      #t.string :email
       t.string :file_name
-      t.datetime :created
       # вроде у них user_id должен появиться
       t.timestamps
     end
--- db/migrate/20211220211630_create_users.rb
+++ db/migrate/20211220211630_create_users.rb
@@ -2,8 +2,8 @@ class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :name
-      t.datetime :created
-      # t.timestamps
+      t.string :email
+      t.timestamps
     end
   end
 end
