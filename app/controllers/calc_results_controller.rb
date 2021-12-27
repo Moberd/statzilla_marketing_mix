fa34bf72d53c512564574b55a56ece84bffdff57
Moberd <60620556+Moberd@users.noreply.github.com>
2021-12-28 00:00:25 +0300

    now accepts only .csv .xls .xlsx files, instead of default pdf script creates pdf with sum pf columns

--- app/controllers/calc_results_controller.rb
+++ app/controllers/calc_results_controller.rb
@@ -1,7 +1,7 @@
 class CalcResultsController < ApplicationController
 
   def index
-    @pdf_attach = File.join("#{Rails.root.to_s}/storage/results/testpdf.pdf")
+    @pdf_attach = File.join("#{Rails.root.to_s}/storage/results/#{params[:calc_results]}")
     send_file(@pdf_attach, filename: "your_document.pdf", disposition: 'inline', type: "application/pdf")
     if params[:calc_results]
       @data = "#{Rails.root.to_s}/storage/results/#{params[:calc_results]}"
--- bin/market_mix_script.py
+++ bin/market_mix_script.py
@@ -1,10 +1,19 @@
 import sys
 import pandas as pd
 import time
+from fpdf import FPDF
+
 if __name__ == "__main__":
     file = sys.argv[1]
-    t1 = pd.read_csv(file, header=0, delimiter=';')
-    res = t1.sum()
-    res.to_csv(f"{sys.argv[4]}/storage/results/{sys.argv[2]}.{sys.argv[3]}.csv",sep='\t',header=False)
-    #time.sleep(10)
-    print(f"{sys.argv[2]}.{sys.argv[3]}.csv analytics calculated")
+    if sys.argv[5] == '.csv':
+        t1 = pd.read_csv(file, header=0, delimiter=';')
+    else:
+        t1 = pd.read_excel(file, header=0)
+    res = t1.sum().to_string()
+    # res.to_csv(f"{sys.argv[4]}/storage/results/{sys.argv[2]}.{sys.argv[3]}.csv",sep='\t',header=False)
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size=12)
+    pdf.cell(200, 10, txt=res, ln=1, align="C")
+    pdf.output(f"{sys.argv[4]}/storage/results/{sys.argv[2]}.{sys.argv[3]}.pdf")
+    print(f"{sys.argv[2]}.{sys.argv[3]}.pdf analytics calculated")
